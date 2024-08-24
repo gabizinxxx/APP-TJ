17ea90fc03c67b9151e1388c76c09ba0f4c6a6d4
gabizinxxx <150867339+gabizinxxx@users.noreply.github.com>
2024-08-24 16:55:30 -0300

    adição de novos arquivos

--- Login/login.php
+++ Login/login.php
@@ -5,7 +5,7 @@ if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    // Conectar ao banco de dados
+    
     $host = 'localhost';
     $user = 'root';
     $senha_user = '';
@@ -13,12 +13,12 @@ if (isset($_POST['submit'])) {
 
     $con = mysqli_connect($host, $user, $senha_user, $banco);
 
-    // Verifique se a conexão foi bem-sucedida
+    
     if (!$con) {
         die("Falha na conexão: " . mysqli_connect_error());
     }
 
-    // Prepare a consulta SQL
+    
     $sql = "SELECT id, senha FROM cadastro WHERE email = ?";
     $stmt = $con->prepare($sql);
 
@@ -29,15 +29,15 @@ if (isset($_POST['submit'])) {
         $stmt->bind_result($id, $senha_hash);
         $stmt->fetch();
 
-        // Adicionar depuração para verificação
+        
         echo "Número de linhas encontradas: " . $stmt->num_rows . "<br>";
         echo "Email: " . $email . "<br>";
         echo "Senha: " . $senha . "<br>";
         echo "Hash de senha no banco de dados: " . $senha_hash . "<br>";
 
-        // Verifique se encontrou um resultado
+        
         if ($stmt->num_rows > 0) {
-            // Verifica se a senha está correta
+           
             if (password_verify($senha, $senha_hash)) {
                 $_SESSION['id'] = $id;
                 echo "Login bem-sucedido, redirecionando...";
@@ -55,7 +55,7 @@ if (isset($_POST['submit'])) {
         echo "Erro na preparação da consulta: " . $con->error;
     }
 
-    // Fechar a conexão
+    
     mysqli_close($con);
 } else {
     echo "O formulário não foi submetido.";
